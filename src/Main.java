683aba23952b3b900cd4a00c07e989866dfe4fe8
griffffon <grisha.zelinskiy@yandex.ru>
2015-01-07 22:59:30 +0200

    test

--- src/Main.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Grigoriy on 07.01.2015.
- */
-public class Main {
-}
---
+++ src/MainClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Grigoriy on 07.01.2015.
+ */
+public class MainClass {
+    public static void Main(String [] args) {
+
+    }
+}
